21c1fd38f39c8b285e27acb5b16da653d9068640
alpha-eng <64243204+alpha-eng@users.noreply.github.com>
2021-01-27 12:01:50 +0600

    diposit button event added

--- app.js
+++ app.js
@@ -6,3 +6,15 @@ loginButton.addEventListener("click",function(){
     document.getElementById("login-area").style.display = "none";
     document.getElementById("transaction-area").style.display = "block";
 })
+
+// diposit button event
+const dipositBtn = document.getElementById("deposit-btn");
+
+dipositBtn.addEventListener("click", function(){
+    const dipositAmount = parseFloat(document.getElementById("deposit-amt").value);
+    const currentDeposit = parseFloat(document.getElementById("curr-deposit").innerHTML);
+    const totalBalance = parseFloat(document.getElementById("total-balance").innerHTML);
+    document.getElementById("curr-deposit").innerHTML = dipositAmount + currentDeposit;
+    document.getElementById("total-balance").innerHTML = dipositAmount + totalBalance;
+    document.getElementById("deposit-amt").value = "";
+})
